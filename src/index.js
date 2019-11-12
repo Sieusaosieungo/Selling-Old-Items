c403b3f49f6042fe1dbab31fc78794193d6536c0
Sieusaosieungo <thinhhanh1@gmail.com>
2019-11-12 20:51:15 +0700

    Add data fake

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const errorHandler = require('./middlewares/errorHanlder');
 
 app.use(express.json());
 app.use(cors());
+
 app.use('/api/users', require('./routes/user.route'));
 app.use('/api/categories', require('./routes/category.route'));
 app.use('/api/products', require('./routes/product.route'));
