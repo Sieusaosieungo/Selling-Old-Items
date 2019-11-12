a0915d8b58a648d511635574e9367cca793a4d77
Sieusaosieungo <thinhhanh1@gmail.com>
2019-11-12 10:51:08 +0700

    add core

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 require('./db/mongoose');
 
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT;
 const errorHandler = require('./middlewares/errorHanlder');
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/users', require('./routes/user.route'));
 app.use('/api/categories', require('./routes/category.route'));
 app.use('/api/products', require('./routes/product.route'));
