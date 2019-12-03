196a640d96b60f289a261ab223973b5642f3fe5a
NightWolf1x <trantrunghuynh1998@gmail.com>
2019-12-03 15:33:59 +0700

    deploy

--- client/server.js
+++ client/server.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
-app.listen(4398);
+app.listen(9000);
